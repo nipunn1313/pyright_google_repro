39be1109e9a3c87d01d779d7d8d3632e4654aaeb
Nipunn Koorapati <nipunn1313@gmail.com>
2021-09-16 15:07:29 -0700

    Comment out the firestore import that doesn't work due to lack of py.typed

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-# firestore doesn't have py.typed - only firestore_v1 has
-from google.cloud.firestore import AsyncClient  # type: ignore
+# firestore doesn't have py.typed yet - only firestore_v1 has
+# from google.cloud.firestore import AsyncClient  # type: ignore
 
 from google.cloud.firestore_v1 import AsyncClient
 
