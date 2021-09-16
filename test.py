b90cf5343d542bcbcc6fd2faf080472b96f232e4
Nipunn Koorapati <nipunn1313@gmail.com>
2021-09-16 15:03:09 -0700

    Improve comments

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+# firestore doesn't have py.typed - only firestore_v1 has
 from google.cloud.firestore import AsyncClient  # type: ignore
+
 from google.cloud.firestore_v1 import AsyncClient
-# from google.protobuf.descriptor import FieldDescriptor
+
+from google.protobuf.descriptor import FieldDescriptor
